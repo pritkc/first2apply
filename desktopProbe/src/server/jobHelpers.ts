973c67e6444bd43863c851b51fb115f114d774bf
sebestindragos <sebestin.dragos@gmail.com>
2024-01-14 20:14:40 +0100

    feat: add we work remotely

--- desktopProbe/src/server/jobHelpers.ts
+++ desktopProbe/src/server/jobHelpers.ts
@@ -3,7 +3,17 @@
  */
 export async function downloadUrl(url: string) {
   console.log(`downloading url: ${url} ...`);
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    headers: {
+      // set the user agent to be the same as the browser
+      // this is needed to prevent some websites from blocking the request
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      Accept: "*/*",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Cache-Control": "no-cache",
+    },
+  });
   const html = await response.text();
   console.log(`finished downloading url: ${url}`);
 
