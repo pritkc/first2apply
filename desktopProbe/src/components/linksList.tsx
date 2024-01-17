43d91ac49f4f42dc7517d6f8e779e748d1f964b0
mariaantici <maria.antici99@gmail.com>
2024-01-18 00:48:18 +0100

    Links page ui and delete unused dashboard

--- desktopProbe/src/components/linksList.tsx
+++ desktopProbe/src/components/linksList.tsx
@@ -4,31 +4,44 @@ import { Button } from "./ui/button";
 
 export function LinksList({ links }: { links: Link[] }) {
   return (
-    <ul className="space-y-10">
-      {links.map((link) => {
-        return (
-          <li key={link.id} className="flex items-center gap-4">
-            <Avatar className="w-11 h-11">
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>LI</AvatarFallback>
-            </Avatar>
+    <section className="space-y-2">
+      <div>
+        <h2 className="text-2xl font-medium tracking-wide">Your searches</h2>
+        <p className="text-muted-foreground text-sm">
+          Below, you'll find your previously saved searches. This is your
+          command center to review and manage them as needed.
+        </p>
+      </div>
+      <hr className="w-full text-muted-foreground" />
+      <ul className="space-y-6 pt-2">
+        {links.map((link) => {
+          return (
+            <li
+              key={link.id}
+              className="flex items-center gap-4 justify-between"
+            >
+              <Avatar className="w-11 h-11">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>LI</AvatarFallback>
+              </Avatar>
 
-            <div className="flex-1 overflow-ellipsis">
-              <p className="font-medium">{link.title}</p>
-              <p className="text-xs text-muted-foreground">
-                {link.url.slice(0, 50)}
-              </p>
-            </div>
+              <div className="flex-1 text-ellipsis overflow-hidden">
+                <p className="font-medium">{link.title}</p>
+                <p className="text-xs text-muted-foreground text-clip whitespace-nowrap">
+                  {link.url}
+                </p>
+              </div>
 
-            {/* actions */}
-            <div className="flex items-center">
-              <Button variant="destructive" size="sm">
-                Delete
-              </Button>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
+              {/* actions */}
+              <div className="flex items-center">
+                <Button variant="destructive" size="sm">
+                  Delete
+                </Button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 }
