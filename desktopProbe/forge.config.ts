4ad5be0b52bd49306e6860f14808d5aa38fee697
sebestindragos <sebestin.dragos@gmail.com>
2024-01-21 20:10:32 +0100

    feat: wip installers

--- desktopProbe/forge.config.ts
+++ desktopProbe/forge.config.ts
@@ -5,9 +5,9 @@ loadEnvVars({ path: path.join(__dirname, "..", "desktopProbe", ".env") });
 
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
 import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
@@ -17,11 +17,23 @@ import { rendererConfig } from "./webpack.renderer.config";
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: path.join(__dirname, "packagers", "icons", "paper-plane"),
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
-    new MakerZIP({}, ["darwin"]),
+    new MakerDMG({
+      format: "ULFO",
+      background: path.join(__dirname, "packagers", "macos-dmg-background.png"),
+      additionalDMGOptions: {
+        window: {
+          size: {
+            width: 540,
+            height: 380,
+          },
+        },
+      },
+    }),
     new MakerRpm({}),
     new MakerDeb({}),
   ],
