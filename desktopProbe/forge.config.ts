397f4481373c61497e29d6c5a059e5cc408d3541
sebestindragos <sebestin.dragos@gmail.com>
2024-02-08 21:49:38 +0100

    chore: osx sign config

--- desktopProbe/forge.config.ts
+++ desktopProbe/forge.config.ts
@@ -25,6 +25,12 @@ const config: ForgeConfig = {
         schemes: ["first2apply"],
       },
     ],
+    osxSign: {},
+    osxNotarize: {
+      appleId: process.env.APPLE_ID ?? "",
+      appleIdPassword: process.env.APPLE_PASSWORD ?? "",
+      teamId: process.env.APPLE_TEAM_ID ?? "",
+    },
   },
   rebuildConfig: {},
   makers: [
