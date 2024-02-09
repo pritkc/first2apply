2bbe322c63151482787727cb987cfdfe66ad15da
mariaantici <maria.antici99@gmail.com>
2024-02-09 15:10:32 +0100

    Fix spinner when saving link

--- desktopProbe/src/components/createLink.tsx
+++ desktopProbe/src/components/createLink.tsx
@@ -8,6 +8,7 @@ import * as z from "zod";
 import { openExternalUrl } from "@/lib/electronMainSdk";
 
 import { Button } from "./ui/button";
+import { Icons } from "./icons";
 import { Input } from "./ui/input";
 import {
   Form,
@@ -83,14 +84,12 @@ export function CreateLink({
               Add new job search
             </FormLabel>
             <FormDescription className="text-sm space-y-3">
-              <p>
-                Name your filtered job searches and provide their website. The
-                more specific your filters, the better we can tailor job alerts
-                for you.
-                <br />
-                Add as many varied searches as you like to maximize your
-                opportunities to be first in line.
-              </p>
+              Name your filtered job searches and provide their website. The
+              more specific your filters, the better we can tailor job alerts
+              for you.
+              <br />
+              Add as many varied searches as you like to maximize your
+              opportunities to be first in line.
               {sites.length > 0 && (
                 <p>
                   Available sites for searches include:{" "}
@@ -166,30 +165,11 @@ export function CreateLink({
             <Button
               type="submit"
               disabled={!form.formState.isValid || isSubmitting}
-              className="w-28 flex justify-center items-center gap-2"
+              className="w-32 flex justify-center items-center gap-2"
             >
               {isSubmitting ? (
                 <>
-                  <svg
-                    className="animate-spin h-4 w-4 text-white flex justify-center"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <circle
-                      className="opacity-25"
-                      cx="12"
-                      cy="12"
-                      r="10"
-                      stroke="currentColor"
-                      stroke-width="4"
-                    ></circle>
-                    <path
-                      className="opacity-75"
-                      fill="currentColor"
-                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    ></path>
-                  </svg>
+                  <Icons.spinner2 className="animate-spin h-4 w-4" />
                   Saving...
                 </>
               ) : (
