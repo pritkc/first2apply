f4093acbf1251ae92b960dcdd44eae7e44f1f986
mariaantici <maria.antici99@gmail.com>
2024-02-18 21:43:43 +0100

    remove isHidden from navbar

--- desktopProbe/src/pages/defaultLayout.tsx
+++ desktopProbe/src/pages/defaultLayout.tsx
@@ -5,17 +5,15 @@ import { Navbar } from "../components/navbar";
  * Default layout for all pages
  */
 export function DefaultLayout({
-  isNavbarHidden,
   className,
   children,
 }: {
-  isNavbarHidden?: boolean;
   className?: string;
   children: React.ReactNode;
 }) {
   return (
     <>
-      <Navbar isHidden={isNavbarHidden} />
+      <Navbar />
       <main className="ml-16 md:ml-20 xl:ml-56">
         <div className={cn("min-h-screen mx-auto max-w-[1536px]", className)}>
           {children}
