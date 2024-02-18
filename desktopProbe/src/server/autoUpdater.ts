c65b81b964bf9e0edc5783914bd8448fcad9cdcb
sebestindragos <sebestin.dragos@gmail.com>
2024-02-18 22:44:10 +0100

    feat: implement auto updater

---
+++ desktopProbe/src/server/autoUpdater.ts
@@ -0,0 +1,45 @@
+import { getExceptionMessage } from "../lib/error";
+import { app, dialog } from "electron";
+import { updateElectronApp, UpdateSourceType } from "update-electron-app";
+
+const S3_BUCKET =
+  "https://s3.eu-central-1.amazonaws.com/first2apply.com/releases";
+
+/**
+ * Class used to handle the auto-updates.
+ */
+export class F2aAutoUpdater {
+  private _canAutoUpdate = true;
+
+  /**
+   * Class constructor.
+   */
+  constructor() {
+    // only enable auto-updates in packaged apps and not for windows
+    this._canAutoUpdate = app.isPackaged && process.platform !== "win32";
+  }
+
+  /**
+   * Start checking for updates.
+   */
+  start() {
+    // if (!this._canAutoUpdate) return;
+
+    updateElectronApp({
+      updateSource: {
+        type: UpdateSourceType.StaticStorage,
+        baseUrl: `${S3_BUCKET}/${process.platform}/${process.arch}`,
+      },
+      updateInterval: "5 minutes",
+      notifyUser: true,
+      logger: console,
+    });
+  }
+
+  /**
+   * Stop checking for updates.
+   */
+  stop() {
+    // this._cronJob?.stop();
+  }
+}
