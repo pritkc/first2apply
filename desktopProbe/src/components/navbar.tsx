218626d27534fcb6cc04d5e9389a7ec0218abba8
mariaantici <maria.antici99@gmail.com>
2024-03-06 22:43:45 +0100

    fix: navbar and tooltip

--- desktopProbe/src/components/navbar.tsx
+++ desktopProbe/src/components/navbar.tsx
@@ -41,62 +41,63 @@ export function Navbar() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <nav className="z-50 fixed h-screen flex flex-col items-center 2xl:items-start border-r border-muted-foreground/20 w-16 2xl:w-56 2xl:pl-10 pt-6 md:p-10 gap-6">
-      <Link to="/" className="flex gap-3 mb-16 md:mb-20">
-        <Icons.logo className="w-7 h-7"></Icons.logo>
-        <span className="hidden 2xl:inline-block text-lg">First 2 Apply</span>
-      </Link>
-      {navItems.map((item) => (
-        <TooltipProvider delayDuration={500} key={item.name}>
-          <Tooltip>
-            <TooltipTrigger>
-              <Link
-                key={item.name}
-                to={item.path}
-                className={`hover:text-primary relative flex items-center gap-3 after:content-[''] after:block after:absolute after:w-0 after:h-0.5 after:bg-primary after:transition-all hover:after:w-full after:right-0 after:bottom-0 after:transition-width duration-200 p-1 ${
-                  location.pathname === item.path && "text-primary"
-                }`}
-              >
-                {item.icon}
-                <span className="hidden 2xl:inline-block text-lg">
-                  {item.name}
-                </span>
-              </Link>
-            </TooltipTrigger>
+    <nav className="z-50 fixed h-screen flex flex-col items-center 2xl:items-start border-r border-muted-foreground/20 w-16 2xl:w-56 py-6 md:p-10 justify-between">
+      <div className="flex flex-col gap-6 items-center">
+        <Link to="/" className="flex gap-3 mb-16 md:mb-20">
+          <Icons.logo className="w-7 h-7"></Icons.logo>
+          <span className="hidden 2xl:inline-block text-lg">First 2 Apply</span>
+        </Link>
 
-            <TooltipContent side="right" className="2xl:hidden text-base">
-              {item.name}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      ))}
+        {navItems.map((item) => (
+          <TooltipProvider delayDuration={500} key={item.name}>
+            <Tooltip>
+              <TooltipTrigger>
+                <Link
+                  key={item.name}
+                  to={item.path}
+                  className={`hover:text-primary relative flex items-center gap-3 after:content-[''] after:block after:absolute after:w-0 after:h-0.5 after:bg-primary after:transition-all hover:after:w-full after:right-0 after:bottom-0 after:transition-width duration-200 p-1 ${
+                    location.pathname === item.path && "text-primary"
+                  }`}
+                >
+                  {item.icon}
+                  <span className="hidden 2xl:inline-block text-lg">
+                    {item.name}
+                  </span>
+                </Link>
+              </TooltipTrigger>
+
+              <TooltipContent side="right" className="2xl:hidden text-base">
+                {item.name}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        ))}
+      </div>
 
       {/* theme toggle */}
-      <div className="align-self-end">
-        <TooltipProvider delayDuration={500}>
-          <Tooltip>
-            <TooltipTrigger>
-              <button
-                onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                className="hover:text-primary relative flex items-center gap-3 after:content-[''] after:block after:absolute after:w-0 after:h-0.5 after:bg-primary after:transition-all hover:after:w-full after:right-0 after:bottom-0 after:transition-width duration-200 p-1"
-              >
-                {theme === "dark" ? (
-                  <SunIcon className="w-7 h-7" />
-                ) : (
-                  <MoonIcon className="w-7 h-7" />
-                )}
-                <span className="hidden 2xl:inline-block text-lg">
-                  {theme === "dark" ? "Light" : "Dark"}
-                </span>
-              </button>
-            </TooltipTrigger>
+      <TooltipProvider delayDuration={500}>
+        <Tooltip>
+          <TooltipTrigger>
+            <button
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+              className="hover:text-primary flex items-center gap-3 p-1"
+            >
+              {theme === "dark" ? (
+                <SunIcon className="w-7 h-7" />
+              ) : (
+                <MoonIcon className="w-7 h-7" />
+              )}
+              <span className="hidden 2xl:inline-block text-lg">
+                {theme === "dark" ? "Light Mode" : "Dark Mode"}
+              </span>
+            </button>
+          </TooltipTrigger>
 
-            <TooltipContent side="right" className="2xl:hidden text-base">
-              {theme === "dark" ? "Light Mode" : "Dark Mode"}
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
+          <TooltipContent side="right" className="2xl:hidden text-base">
+            {theme === "dark" ? "Light Mode" : "Dark Mode"}
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </nav>
   );
 }
