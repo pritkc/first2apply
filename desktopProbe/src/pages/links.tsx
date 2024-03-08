0cd203d735fec5aaa101168267149760977956a4
mariaantici <maria.antici99@gmail.com>
2024-03-08 22:56:53 +0100

    fix: no links ui

--- desktopProbe/src/pages/links.tsx
+++ desktopProbe/src/pages/links.tsx
@@ -1,5 +1,6 @@
 import { useError } from "@/hooks/error";
 import { useLinks } from "@/hooks/links";
+
 import { InfoCircledIcon } from "@radix-ui/react-icons";
 
 import { DefaultLayout } from "./defaultLayout";
@@ -43,25 +44,40 @@ export function LinksPage() {
       </div>
 
       {links.length === 0 && (
-        <div className="flex items-center justify-center w-full lg:h-[calc(100vh-160px)]">
-          <div className="flex items-center flex-col lg:flex-row gap-10 xl:gap-20">
-            <div className="lg:w-2/5">
-              <h2 className="text-xl xl:text-2xl whitespace-break-spaces tracking-wide break-normal mt-6 lg:mt-0">
-                <span className="font-medium">First 2 Apply</span> periodically
-                visits your{" "}
-                <span className="underline whitespace-nowrap">
-                  pre-configured
-                </span>{" "}
-                job searches and fetches the list of jobs. If there are new jobs
-                since the last visit, you will be notified.
-              </h2>
+        <>
+          <div className="flex items-center justify-between">
+            <h2 className="w-3/5 xl:w-1/2 text-xl md:text-2xl my-6 lg:my-10 whitespace-break-spaces tracking-wide break-normal">
+              First 2 Apply periodically visits your{" "}
+              <span className="font-medium whitespace-nowrap">
+                pre-configured
+              </span>{" "}
+              job searches and fetches the list of jobs. If there are new jobs
+              since the last visit, you will be notified.
+            </h2>
 
-              <p className="mt-1 lg:mt-4 mb-10 lg:mb-6">
-                There is no magic involved, undearneath it's just a simple web
-                browser that loads the page and extracts the data.
+            <div className="w-fit lg:hidden">
+              <CreateLink />
+            </div>
+          </div>
+
+          <div className="flex items-start flex-col-reverse lg:flex-row gap-6 lg:gap-10">
+            <div className="lg:w-2/5 space-y-6 xl:space-y-12">
+              <p className="text-justify">
+                To optimize your job search and achieve the best results, the
+                video displayed on the right will guide you through configuring
+                a job search on LinkedIn. After setting up your search criteria
+                to match your preferences and career goals, simply copy and
+                paste the URL from the job platform into the app.
               </p>
 
-              <Alert className="flex items-center gap-2 border-0 p-0">
+              <p className="text-justify">
+                This process ensures that we can provide you with the most
+                relevant and effective job search results. There is no magic
+                involved, undearneath it's just a simple web browser that loads
+                the page and extracts the data.
+              </p>
+
+              <Alert className="flex items-center  gap-2 border-0 p-0">
                 <AlertTitle className="mb-0">
                   <InfoCircledIcon className="w-5 h-5" />
                 </AlertTitle>
@@ -71,24 +87,24 @@ export function LinksPage() {
                 </AlertDescription>
               </Alert>
 
-              <div className="mx-auto w-fit mt-10">
+              <div className="mx-auto w-fit hidden lg:block">
                 <CreateLink />
               </div>
             </div>
 
-            <div className="mx-auto mb-6 lg:w-3/5 space-y-4">
+            <div className="mx-auto lg:w-3/5 space-y-4">
               <img
                 src="https://vnawaforiamopaudfefi.supabase.co/storage/v1/object/public/first2apply-public/copy_link.gif"
                 alt="LI job search config"
-                className="w-full rounded-lg"
+                className="w-full rounded-xl max-w-"
               />
-              <p className="text-center">
+              <p className="text-center text-sm">
                 How to configure a job search on LinkedIn and copy paste the URL
                 in the app
               </p>
             </div>
           </div>
-        </div>
+        </>
       )}
 
       {links.length > 0 && (
