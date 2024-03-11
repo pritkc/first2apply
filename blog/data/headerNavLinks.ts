47c7ea8a6832552a0821cc9925a59233aee5009e
sebestindragos <sebestin.dragos@gmail.com>
2024-03-11 21:28:40 +0100

    fix: move blog pages top level

--- blog/data/headerNavLinks.ts
+++ blog/data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
+  { href: '/articles', title: 'Articles' },
   { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
--- blog/layouts/PostLayout.tsx
+++ blog/layouts/PostLayout.tsx
@@ -31,7 +31,6 @@ interface LayoutProps {
 
 export default function PostLayout({ content, authorDetails, next, prev, children }: LayoutProps) {
   const { filePath, path, slug, date, title, tags } = content
-  const basePath = path.split('/')[0]
 
   return (
     <SectionContainer>
@@ -150,7 +149,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
               </div>
               <div className="pt-4 xl:pt-8">
                 <Link
-                  href={`/${basePath}`}
+                  href={`/articles`}
                   className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                   aria-label="Back to the blog"
                 >
