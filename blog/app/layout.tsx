a80cc7a4769c2ab0e8d04f1d88afaa19ef012197
sebestindragos <sebestin.dragos@gmail.com>
2024-03-11 23:34:48 +0100

    fix(blog): search component

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -3,13 +3,14 @@ import 'pliny/search/algolia.css'
 
 import { Space_Grotesk } from 'next/font/google'
 import { Analytics, AnalyticsConfig } from 'pliny/analytics'
-import { SearchProvider, SearchConfig } from 'pliny/search'
+import { SearchConfig } from 'pliny/search'
 import Header from '@/components/Header'
 import SectionContainer from '@/components/SectionContainer'
 import Footer from '@/components/Footer'
 import siteMetadata from '@/data/siteMetadata'
 import { ThemeProviders } from './theme-providers'
 import { Metadata } from 'next'
+import { SearchProvider } from '@/components/SearchProvider'
 
 const space_grotesk = Space_Grotesk({
   subsets: ['latin'],
@@ -92,7 +93,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
           <SectionContainer>
             <div className="flex h-screen flex-col justify-between font-sans">
-              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+              <SearchProvider>
                 <Header />
                 <main className="mb-auto">{children}</main>
               </SearchProvider>
