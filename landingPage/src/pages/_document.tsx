0d07daaee97e9e83da8bdfb23d66a3cf9b05f1af
sebestindragos <sebestin.dragos@gmail.com>
2024-03-22 09:40:39 +0100

    feat: project boilerplate

---
+++ landingPage/src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider } from "@/components/themeProvider";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Main />
+        </ThemeProvider>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
