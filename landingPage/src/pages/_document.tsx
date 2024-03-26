49b2a47a13d0e0475e6ae395862556e3506052d5
mariaantici <maria.antici99@gmail.com>
2024-03-26 20:46:01 +0100

    feat: add first sections of landing page

--- landingPage/src/pages/_document.tsx
+++ landingPage/src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default function Document() {
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
--- landingPage/src/pages/index.tsx
+++ landingPage/src/pages/index.tsx
@@ -1,127 +1,104 @@
+import { DefaultLayout } from "@/components/defaultLayout";
 import Image from "next/image";
-import { Inter } from "next/font/google";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Terminal } from "lucide-react";
-
-const inter = Inter({ subsets: ["latin"] });
+import trackBlackImage from "../../public/assets/trackBlack.png";
+import trackWhiteImage from "../../public/assets/trackWhite.png";
+import screenshotLight from "../../public/assets/screenshotLight.png";
+import screenshotDark from "../../public/assets/screenshotDark.png";
+import { Button } from "@/components/ui/button";
+import { QuoteIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <Alert>
-          <Terminal className="h-4 w-4" />
-          <AlertTitle>Heads up!</AlertTitle>
-          <AlertDescription>
-            You can add components and dependencies to your app using the cli.
-          </AlertDescription>
-        </Alert>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <DefaultLayout>
+      <section className="max-w-7xl h-[50vh] mx-auto px-10 w-full flex items-end justify-between gap-20 pt-[calc(50vh-96px)] lg:pt-[calc(50vh-120px)]">
+        <div id="embed02" className="absolute top-10">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+            href="https://www.producthunt.com/posts/first-2-apply?utm_source=badge-featured&amp;utm_medium=badge&amp;utm_souce=badge-first-2-apply"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <img
+              src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=443975&amp;theme=dark"
+              alt="First 2 Apply - New job alerts from 10+ most popular sites  | Product Hunt"
+              width="250"
+              height="54"
             />
           </a>
         </div>
-      </div>
+        <h1 className="z-50 text-5xl lg:text-6xl font-semibold text-nowrap">
+          Never miss a job
+          <br />
+          opportunity again.
+        </h1>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <Button className="w-full max-w-72 lg:max-w-96 h-12 lg:h-14 text-xl lg:text-2xl text-background">
+          Try it now for free
+        </Button>
+      </section>
+
+      <section className="relative h-[50vh] bg-gradient-to-t from-muted dark:from-card/60 to-muted/40 dark:to-card/20">
+        <div className="max-w-7xl mx-auto px-10 pt-4">
+          <p className="text-sm text-foreground/70 max-w-[500px] text-balance">
+            Let <span className="text-foreground">first 2 apply</span> transform
+            your job search. Track new openings from multiple platforms in one
+            place, and manage your applications effortlessly.
+          </p>
+
+          <Image
+            src={trackBlackImage}
+            alt="paperfly track black"
+            className="z-50 dark:hidden absolute top-0 md:left-1/2 md:-translate-x-1/2 h-auto lg:h-[40vh] max-h-[315px] w-96 lg:w-auto md:ml-28 lg:ml-8"
+          />
+          <Image
+            src={trackWhiteImage}
+            alt="paperfly track white"
+            className="z-50 hidden dark:block absolute top-0 md:left-1/2 md:-translate-x-1/2 h-auto lg:h-[40vh] max-h-[315px] w-96 lg:w-auto md:ml-28 lg:ml-8"
+          />
+        </div>
+      </section>
+
+      <section className="relative -top-[20vh] max-w-5xl mx-auto rounded-2xl">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={screenshotLight}
+          alt="app homepage light"
+          className="dark:hidden w-full h-auto"
         />
-      </div>
+        <Image
+          src={screenshotDark}
+          alt="app homepage dark"
+          className="hidden dark:block w-full h-auto"
+        />
+      </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="max-w-7xl mx-auto px-10 min-h-screen">
+        <h2 className="text-4xl font-semibold text-center">
+          Stay Ahead of the Competition with First 2 Apply
+        </h2>
+      </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+      <section className="bg-muted dark:bg-card/60">
+        <div className="max-w-5xl mx-auto pt-40 pb-24">
+          <QuoteIcon className="w-16 h-16 text-foreground/80" />
+          <p className="text-xl px-16 text-balance">
+            Helped me find a job within a few days of installing!
+            <br />
+            <br />
+            After being able to only send out about 5-10 applications a day,
+            which usually took me about 3 hours of parsing through a bunch of
+            suggestions that simply weren't relevant to me, this app immensely
+            increased my productivity! I was able to send out about 30-40
+            applications most days, in the course of about 2 hours. It also
+            found jobs that I would've otherwise not seen! But most importantly,
+            it allowed me to be one of the first applicants (I believe I was
+            within the first 5) to a particular job, which I am sure was
+            instrumental in me then landing that position!
           </p>
-        </a>
+          <QuoteIcon className="w-16 h-16 text-foreground/80 ml-auto" />
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
+          <p className="mt-12 text-right font-semibold text-base leading-5">
+            Source: MS Store Reviews
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </div>
+      </section>
+    </DefaultLayout>
   );
 }
