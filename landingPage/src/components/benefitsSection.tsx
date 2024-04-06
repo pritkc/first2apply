45cb2cf36e4e1f4d561e09a5f3ffce1714640029
mariaantici <maria.antici99@gmail.com>
2024-04-07 00:32:24 +0200

    feat: finalize all sections

---
+++ landingPage/src/components/benefitsSection.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import Image from "next/image";
+import benefitsLight from "../../public/assets/benefits-light.png";
+import benefitsDark from "../../public/assets/benefits-dark.png";
+
+const sites = [
+  "LinkedIn",
+  "Indeed",
+  "Dice",
+  "Glassdoor",
+  "BuildIn",
+  "Remotive",
+  "Remote IO",
+  "RemoteOK",
+  "WeWorkRemotely",
+  "FlexJobs",
+  "BestJobs",
+  "EchoJobs",
+  "Robert Half",
+];
+
+export function BenefitsSection() {
+  const [showAllPlatforms, setShowAllPlatforms] = useState(false);
+
+  return (
+    <section
+      id="benefits"
+      className="flex flex-col items-center gap-4 mt-[10vh] mx-auto max-w-7xl px-6 sm:px-10 md:flex-row"
+    >
+      <div className="w-full md:w-1/2">
+        <h2 className="text-2xl font-semibold sm:text-4xl text-balance">
+          All Your Favorite Job Sites in One Place
+        </h2>
+
+        <p className="mt-2 sm:mt-4 text-balance">
+          Venture beyond LinkedIn to seize every opportunity. But manual
+          searching across multiple platforms can be overwhelming... Let First 2
+          Apply automate the process, fetching roles from{" "}
+          {showAllPlatforms
+            ? sites.join(", ")
+            : `${sites.slice(0, 3).join(", ")}, and `}
+          {!showAllPlatforms && (
+            <button
+              onClick={() => setShowAllPlatforms(true)}
+              className="text-blue-500"
+            >
+              {sites.length - 3} other job platforms
+            </button>
+          )}
+          . Just sit back, relax and let us curate the perfect job list for you.
+        </p>
+      </div>
+
+      <div className="w-full h-auto md:w-1/2">
+        <Image
+          src={benefitsLight}
+          alt="sites light"
+          className="dark:hidden w-full h-auto"
+        />
+        <Image
+          src={benefitsDark}
+          alt="sites dark"
+          className="hidden dark:block w-full h-auto"
+        />
+      </div>
+    </section>
+  );
+}
---
+++ landingPage/src/components/pricingSection.tsx
@@ -0,0 +1,164 @@
+import { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { CheckCircledIcon } from "@radix-ui/react-icons";
+
+const tabs = ["Monthly", "Quarterly", "Biannually", "Yearly"];
+
+type PriceDetail = {
+  pricePerMonth: number;
+  total: number;
+  discount: number;
+  period: number;
+};
+
+type PricingPlan = {
+  name: string;
+  description?: string;
+  monthly: PriceDetail;
+  quarterly: PriceDetail;
+  biannually: PriceDetail;
+  yearly: PriceDetail;
+  benefits: string[];
+};
+
+const pricingPlans: PricingPlan[] = [
+  {
+    name: "Basic",
+    description: "Cheap as a beer",
+    monthly: { pricePerMonth: 5, total: 5, discount: 0, period: 1 },
+    quarterly: { pricePerMonth: 3.75, total: 11.25, discount: 0.25, period: 3 },
+    biannually: { pricePerMonth: 3.25, total: 19.5, discount: 0.35, period: 6 },
+    yearly: { pricePerMonth: 2.5, total: 30, discount: 0.5, period: 12 },
+    benefits: [
+      "Unlimited job site monitoring.",
+      "Instant new job alerts.",
+      "Application organization.",
+    ],
+  },
+  {
+    name: "Pro",
+    description: "All you need to get hired",
+    monthly: { pricePerMonth: 10, total: 10, discount: 0, period: 1 },
+    quarterly: { pricePerMonth: 7.5, total: 22.5, discount: 0.25, period: 3 },
+    biannually: { pricePerMonth: 6.5, total: 39, discount: 0.35, period: 6 },
+    yearly: { pricePerMonth: 5, total: 60, discount: 0.5, period: 12 },
+    benefits: [
+      "Everything the basic plan offers.",
+      "Blacklist companies.",
+      "Advanced filtering.",
+    ],
+  },
+];
+
+export function PricingSection() {
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+
+  return (
+    <section
+      id="pricing"
+      className="max-w-5xl mx-auto px-6 sm:px-10 pt-[10vh] md:pt-[20vh]"
+    >
+      <h2 className="text-2xl sm:text-4xl lg:text-5xl font-semibold md:text-center">
+        Pricing Plans
+      </h2>
+      <p className="mt-[3vh] sm:mt-[5vh] mb-3 text-center">Billing Period</p>
+      <Tabs defaultValue={tabs[0]} className="flex flex-col items-center">
+        <TabsList className="w-fit">
+          {tabs.map((tab) => (
+            <TabsTrigger
+              key={tab}
+              value={tab}
+              onClick={() => {
+                setSelectedTab(tab);
+              }}
+            >
+              {tab}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        <div className="mt-[5vh] sm:mt-[10vh] w-full grid sm:grid-cols-2 gap-4 md:gap-10 lg:gap-20">
+          {pricingPlans.map((plan) => (
+            <Card key={plan.name} className="relative overflow-hidden">
+              <CardHeader className="md:p-8">
+                <CardTitle className="md:text-3xl">{plan.name}</CardTitle>
+                <CardDescription>{plan.description}</CardDescription>
+              </CardHeader>
+              <CardContent className="md:p-8 md:pt-2">
+                <TabsContent value={selectedTab}>
+                  <p>
+                    <span className="text-2xl md:text-3xl font-semibold">
+                      $
+                    </span>
+                    <span className="text-4xl md:text-5xl font-bold">
+                      {
+                        (
+                          plan[
+                            selectedTab.toLowerCase() as keyof PricingPlan
+                          ] as PriceDetail
+                        ).pricePerMonth
+                      }
+                    </span>
+                    <span className="text-sm"> /month</span>
+                    {selectedTab !== "Monthly" && (
+                      <div className="ml-4 inline-block relative">
+                        <span className="text-2xl md:text-3xl text-muted-foreground/30 font-semibold">
+                          $
+                        </span>
+                        <span className="text-4xl md:text-5xl text-muted-foreground/30 font-bold">
+                          {plan.monthly.pricePerMonth}
+                        </span>
+                        <span className="text-sm text-muted-foreground/30">
+                          /month
+                        </span>
+                        <div className="absolute top-5 md:top-7 w-full border border-t-1 border-muted-foreground/30 dark:border-muted-foreground/60 rotate-12"></div>
+                      </div>
+                    )}
+                  </p>
+                  <p className="mt-1 sm:mt-2 h-6 text-muted-foreground">
+                    {selectedTab !== "Monthly" && (
+                      <>
+                        Pay $
+                        {
+                          (
+                            plan[
+                              selectedTab.toLowerCase() as keyof PricingPlan
+                            ] as PriceDetail
+                          ).total
+                        }{" "}
+                        {selectedTab.toLowerCase()}
+                      </>
+                    )}
+                  </p>
+                </TabsContent>
+              </CardContent>
+              <CardFooter className="md:p-8 md:pt-0">
+                <ul>
+                  {plan.benefits.map((benefit, index) => (
+                    <li key={index} className="flex items-center gap-2">
+                      <CheckCircledIcon className="w-5 h-5 text-primary" />
+                      <span className="md:text-lg">{benefit}</span>
+                    </li>
+                  ))}
+                </ul>
+              </CardFooter>
+
+              {plan.name === "Pro" && (
+                <div className="absolute top-12 -right-2 bg-primary text-background sm:text-lg font-medium py-1 sm:py-2 px-20 transform rotate-45 translate-x-1/4 -translate-y-1/4">
+                  Coming Soon
+                </div>
+              )}
+            </Card>
+          ))}
+        </div>
+      </Tabs>
+    </section>
+  );
+}
