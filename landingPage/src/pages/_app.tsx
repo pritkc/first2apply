c864b4ee53deb8b8b1a0eaeb3d55f7dcf8af98a4
sebestindragos <sebestin.dragos@gmail.com>
2024-04-10 18:21:17 +0200

    feat(landingPage): add ga4 tracking

--- landingPage/src/pages/_app.tsx
+++ landingPage/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-YZM4X6MLS6" />
+    </>
+  );
 }
--- landingPage/src/pages/download.tsx
+++ landingPage/src/pages/download.tsx
@@ -1,5 +1,6 @@
 import { DefaultLayout } from "@/components/defaultLayout";
 import { Button } from "@/components/ui/button";
+import { sendGAEvent } from "@next/third-parties/google";
 
 export default function Download() {
   return (
@@ -14,8 +15,18 @@ export default function Download() {
           payment required
         </h2>
 
-        <div className="mt-12 w-full xs:max-w-96 flex flex-col xs:flex-row xs:flex-wrap xs:items-center xs:justify-center gap-4">
-          <a href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/darwin/arm64/First+2+Apply-1.3.0-arm64.dmg">
+        <div className="mt-12 w-full xs:max-w-[500px] flex flex-col xs:flex-row xs:flex-wrap xs:items-center xs:justify-center gap-4">
+          <a
+            href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/darwin/arm64/First+2+Apply-1.3.2-arm64.dmg"
+            onClick={() =>
+              sendGAEvent({
+                event: "file_download",
+                file_name: "First 2 Apply-1.3.2-arm64",
+                file_extension: "dmg",
+                link_domain: "first2apply.com",
+              })
+            }
+          >
             <Button
               size="lg"
               className="h-12 w-full xs:w-fit flex items-center gap-2"
@@ -36,74 +47,16 @@ export default function Download() {
             </Button>
           </a>
           <a
-            href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/linux/x64/first-2-apply_1.3.0_amd64.deb"
-            className="hidden xs:block"
+            href="ms-windows-store://pdp/?productid=9NK18WV87SV2"
+            onClick={() =>
+              sendGAEvent({
+                event: "file_download",
+                file_name: "First 2 Apply-1.3.2",
+                file_extension: "exe",
+                link_domain: "first2apply.com",
+              })
+            }
           >
-            <Button
-              size="lg"
-              className="h-12 w-full xs:w-fit flex items-center gap-2"
-            >
-              <svg
-                className="dark:hidden w-5 h-auto"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#000000"
-              >
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  <title>linux</title>{" "}
-                  <rect width="24" height="24" fill="none"></rect>{" "}
-                  <path d="M14.62,8.35c-.42.28-1.75,1-1.95,1.19a.82.82,0,0,1-1.14,0c-.2-.16-1.53-.92-1.95-1.19s-.45-.7.08-.92a6.16,6.16,0,0,1,4.91,0c.49.21.51.6,0,.9m7.22,7.28A19.09,19.09,0,0,0,18,10a4.31,4.31,0,0,1-1.06-1.88c-.1-.33-.17-.67-.24-1A11.32,11.32,0,0,0,16,4.47,4.06,4.06,0,0,0,12.16,2,4.2,4.2,0,0,0,8.21,4.4a5.9,5.9,0,0,0-.46,1.34c-.17.76-.32,1.55-.5,2.32a3.38,3.38,0,0,1-1,1.71,19.53,19.53,0,0,0-3.88,5.35A6.09,6.09,0,0,0,2,16c-.19.66.29,1.12,1,1,.44-.09.88-.18,1.3-.31s.57,0,.67.35a6.73,6.73,0,0,0,4.24,4.5c4.12,1.56,8.93-.66,10-4.58.07-.27.17-.37.47-.27.46.14.93.24,1.4.35a.72.72,0,0,0,.92-.64,1.44,1.44,0,0,0-.16-.73"></path>{" "}
-                </g>
-              </svg>
-              <svg
-                className="hidden dark:block w-5 h-auto"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#ffffff"
-              >
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g
-                  id="SVGRepo_tracerCarrier"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                ></g>
-                <g id="SVGRepo_iconCarrier">
-                  {" "}
-                  <title>linux</title>{" "}
-                  <rect width="24" height="24" fill="none"></rect>{" "}
-                  <path d="M14.62,8.35c-.42.28-1.75,1-1.95,1.19a.82.82,0,0,1-1.14,0c-.2-.16-1.53-.92-1.95-1.19s-.45-.7.08-.92a6.16,6.16,0,0,1,4.91,0c.49.21.51.6,0,.9m7.22,7.28A19.09,19.09,0,0,0,18,10a4.31,4.31,0,0,1-1.06-1.88c-.1-.33-.17-.67-.24-1A11.32,11.32,0,0,0,16,4.47,4.06,4.06,0,0,0,12.16,2,4.2,4.2,0,0,0,8.21,4.4a5.9,5.9,0,0,0-.46,1.34c-.17.76-.32,1.55-.5,2.32a3.38,3.38,0,0,1-1,1.71,19.53,19.53,0,0,0-3.88,5.35A6.09,6.09,0,0,0,2,16c-.19.66.29,1.12,1,1,.44-.09.88-.18,1.3-.31s.57,0,.67.35a6.73,6.73,0,0,0,4.24,4.5c4.12,1.56,8.93-.66,10-4.58.07-.27.17-.37.47-.27.46.14.93.24,1.4.35a.72.72,0,0,0,.92-.64,1.44,1.44,0,0,0-.16-.73"></path>{" "}
-                </g>
-              </svg>
-              Linux
-            </Button>
-          </a>
-          <a href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/darwin/x64/First+2+Apply-1.3.0-x64.dmg">
-            <Button
-              size="lg"
-              className="h-12 w-full xs:w-fit flex items-center gap-2"
-            >
-              <img
-                width="16"
-                alt="Apple logo white"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Apple_logo_white.svg/256px-Apple_logo_white.svg.png"
-                className="h-4 w-auto hidden dark:block"
-              />
-              <img
-                width="16"
-                alt="Apple logo black"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/256px-Apple_logo_black.svg.png"
-                className="h-4 w-auto dark:hidden"
-              />
-              MacOS Intel
-            </Button>
-          </a>
-          <a href="ms-windows-store://pdp/?productid=9NK18WV87SV2">
             <Button
               size="lg"
               className="h-12 w-full xs:w-fit flex items-center gap-2"
@@ -123,9 +76,50 @@ export default function Download() {
               Windows
             </Button>
           </a>
+
+          {/* macos x64 */}
           <a
-            href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/linux/x64/first-2-apply_1.3.0_amd64.deb"
-            className="xs:hidden"
+            href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/darwin/x64/First+2+Apply-1.3.2-x64.dmg"
+            onClick={() =>
+              sendGAEvent({
+                event: "file_download",
+                file_name: "First 2 Apply-1.3.2-x64",
+                file_extension: "dmg",
+                link_domain: "first2apply.com",
+              })
+            }
+          >
+            <Button
+              size="lg"
+              className="h-12 w-full xs:w-fit flex items-center gap-2"
+            >
+              <img
+                width="16"
+                alt="Apple logo white"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Apple_logo_white.svg/256px-Apple_logo_white.svg.png"
+                className="h-4 w-auto hidden dark:block"
+              />
+              <img
+                width="16"
+                alt="Apple logo black"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/256px-Apple_logo_black.svg.png"
+                className="h-4 w-auto dark:hidden"
+              />
+              MacOS Intel
+            </Button>
+          </a>
+
+          <a
+            href="https://s3.eu-central-1.amazonaws.com/first2apply.com/releases/linux/x64/first-2-apply_1.3.2_amd64.deb"
+            className="hidden xs:block"
+            onClick={() =>
+              sendGAEvent({
+                event: "file_download",
+                file_name: "First 2 Apply-1.3.2-amd64",
+                file_extension: "deb",
+                link_domain: "first2apply.com",
+              })
+            }
           >
             <Button
               size="lg"
