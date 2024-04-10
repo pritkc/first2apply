c173156a0a390db8b8bdc51d4a14f9677bbe9dd1
sebestindragos <sebestin.dragos@gmail.com>
2024-04-10 18:31:18 +0200

    fix(landingPage): dark mode

--- landingPage/src/pages/_app.tsx
+++ landingPage/src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { ThemeProvider } from "@/components/themeProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
       <GoogleAnalytics gaId="G-YZM4X6MLS6" />
     </>
   );
