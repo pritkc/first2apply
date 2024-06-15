2bc0b8a8b53577c75ba4fb43460221ae272b8ae5
sebestindragos <sebestin.dragos@gmail.com>
2024-06-15 13:42:17 +0200

    feat: add filters page

--- desktopProbe/src/components/navbar.tsx
+++ desktopProbe/src/components/navbar.tsx
@@ -1,43 +1,45 @@
-import { Link, useLocation } from "react-router-dom";
-import { Icons } from "@/components/icons";
+import { Icons } from '@/components/icons';
 import {
   ChatBubbleIcon,
+  Crosshair2Icon,
   GearIcon,
   HomeIcon,
   MagnifyingGlassIcon,
   MoonIcon,
   QuestionMarkCircledIcon,
   SunIcon,
-} from "@radix-ui/react-icons";
-import {
-  Tooltip,
-  TooltipProvider,
-  TooltipTrigger,
-  TooltipContent,
-} from "./ui/tooltip";
-import { useTheme } from "next-themes";
+} from '@radix-ui/react-icons';
+import { useTheme } from 'next-themes';
+import { Link, useLocation } from 'react-router-dom';
+
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
 
 const navItems = [
-  { name: "Jobs", path: "/", icon: <HomeIcon className="w-7 h-7" /> },
+  { name: 'Jobs', path: '/', icon: <HomeIcon className="h-7 w-7" /> },
   {
-    name: "Searches",
-    path: "/links",
-    icon: <MagnifyingGlassIcon className="w-7 h-7" />,
+    name: 'Searches',
+    path: '/links',
+    icon: <MagnifyingGlassIcon className="h-7 w-7" />,
   },
   {
-    name: "Settings",
-    path: "/settings",
-    icon: <GearIcon className="w-7 h-7" />,
+    name: 'Filters',
+    path: '/filters',
+    icon: <Crosshair2Icon className="h-7 w-7" />,
   },
   {
-    name: "Feedback",
-    path: "/feedback",
-    icon: <ChatBubbleIcon className="w-7 h-7" />,
+    name: 'Feedback',
+    path: '/feedback',
+    icon: <ChatBubbleIcon className="h-7 w-7" />,
   },
   {
-    name: "Help",
-    path: "/help",
-    icon: <QuestionMarkCircledIcon className="w-7 h-7" />,
+    name: 'Settings',
+    path: '/settings',
+    icon: <GearIcon className="h-7 w-7" />,
+  },
+  {
+    name: 'Help',
+    path: '/help',
+    icon: <QuestionMarkCircledIcon className="h-7 w-7" />,
   },
 ];
 
@@ -47,11 +49,11 @@ export function Navbar() {
   const { theme, setTheme } = useTheme();
 
   return (
-    <nav className="z-50 fixed h-screen flex flex-col items-center 2xl:items-start border-r border-muted-foreground/20 w-16 2xl:w-56 py-6 md:p-10 justify-between">
-      <div className="flex flex-col gap-6 items-center 2xl:items-start">
-        <Link to="/" className="flex gap-3 mb-16 md:mb-20">
-          <Icons.logo className="w-7 h-7"></Icons.logo>
-          <span className="hidden 2xl:inline-block text-lg">First 2 Apply</span>
+    <nav className="fixed z-50 flex h-screen w-16 flex-col items-center justify-between border-r border-muted-foreground/20 py-6 md:p-10 2xl:w-56 2xl:items-start">
+      <div className="flex flex-col items-center gap-6 2xl:items-start">
+        <Link to="/" className="mb-16 flex gap-3 md:mb-20">
+          <Icons.logo className="h-7 w-7"></Icons.logo>
+          <span className="hidden text-lg 2xl:inline-block">First 2 Apply</span>
         </Link>
 
         {navItems.map((item) => (
@@ -61,18 +63,16 @@ export function Navbar() {
                 <Link
                   key={item.name}
                   to={item.path}
-                  className={`hover:text-primary relative flex items-center gap-3 after:content-[''] after:block after:absolute after:w-0 after:h-0.5 after:bg-primary after:transition-all hover:after:w-full after:right-0 after:bottom-0 after:transition-width duration-200 p-1 ${
-                    location.pathname === item.path && "text-primary"
+                  className={`after:transition-width relative flex items-center gap-3 p-1 duration-200 after:absolute after:bottom-0 after:right-0 after:block after:h-0.5 after:w-0 after:bg-primary after:transition-all after:content-[''] hover:text-primary hover:after:w-full ${
+                    location.pathname === item.path && 'text-primary'
                   }`}
                 >
                   {item.icon}
-                  <span className="hidden 2xl:inline-block text-lg">
-                    {item.name}
-                  </span>
+                  <span className="hidden text-lg 2xl:inline-block">{item.name}</span>
                 </Link>
               </TooltipTrigger>
 
-              <TooltipContent side="right" className="2xl:hidden text-base">
+              <TooltipContent side="right" className="text-base 2xl:hidden">
                 {item.name}
               </TooltipContent>
             </Tooltip>
@@ -85,22 +85,16 @@ export function Navbar() {
         <Tooltip>
           <TooltipTrigger>
             <button
-              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-              className="hover:text-primary flex items-center gap-3 p-1"
+              onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+              className="flex items-center gap-3 p-1 hover:text-primary"
             >
-              {theme === "dark" ? (
-                <SunIcon className="w-7 h-7" />
-              ) : (
-                <MoonIcon className="w-7 h-7" />
-              )}
-              <span className="hidden 2xl:inline-block text-lg">
-                {theme === "dark" ? "Light Mode" : "Dark Mode"}
-              </span>
+              {theme === 'dark' ? <SunIcon className="h-7 w-7" /> : <MoonIcon className="h-7 w-7" />}
+              <span className="hidden text-lg 2xl:inline-block">{theme === 'dark' ? 'Light Mode' : 'Dark Mode'}</span>
             </button>
           </TooltipTrigger>
 
-          <TooltipContent side="right" className="2xl:hidden text-base">
-            {theme === "dark" ? "Light Mode" : "Dark Mode"}
+          <TooltipContent side="right" className="text-base 2xl:hidden">
+            {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
---
+++ desktopProbe/src/pages/filters.tsx
@@ -0,0 +1,11 @@
+import { DefaultLayout } from "./defaultLayout";
+
+export function FiltersPage() {
+  return (
+    <DefaultLayout className="p-6 md:p-10 space-y-6 flex flex-col">
+      <h1 className="text-2xl font-medium tracking-wide w-fit">
+        Advanced Matching
+      </h1>
+    </DefaultLayout>
+  );
+}
