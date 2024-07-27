9f4c002ef36c5afabeea5b5fd5f2b709f67877c7
sebestindragos <sebestin.dragos@gmail.com>
2024-07-27 12:28:07 +0200

    fix: do not process links for users with expired subscriptions

---
+++ supabase/functions/_shared/subscription.ts
@@ -0,0 +1,42 @@
+import { SupabaseClient } from "https://esm.sh/@supabase/supabase-js@2.39.0";
+import { DbSchema, Profile } from "./types.ts";
+
+/**
+ * Retrieve the user profile and check if his subscription allows advanced matching.
+ */
+export async function checkUserSubscription({
+  supabaseClient,
+  userId,
+}: {
+  supabaseClient: SupabaseClient<DbSchema, "public">;
+  userId: string;
+}): Promise<{
+  profile: Profile;
+  subscriptionHasExpired: boolean;
+  hasAdvancedMatching: boolean;
+}> {
+  const { data: profile, error } = await supabaseClient
+    .from("profiles")
+    .select("*")
+    .eq("user_id", userId)
+    .single();
+
+  if (error) {
+    throw error;
+  }
+
+  if (!profile) {
+    throw new Error("Profile not found");
+  }
+
+  // check if the user's subscription has expired
+  const subscriptionHasExpired =
+    new Date(profile.subscription_end_date) < new Date();
+  const hasRequiredTier = profile.subscription_tier === "pro";
+
+  return {
+    profile,
+    subscriptionHasExpired,
+    hasAdvancedMatching: hasRequiredTier && !subscriptionHasExpired,
+  };
+}
