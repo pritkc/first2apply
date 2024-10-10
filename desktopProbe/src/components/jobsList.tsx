b7367073d559f4c35e5a5566acdef3c82b6939c9
mariaantici <maria.antici99@gmail.com>
2024-10-10 18:11:24 +0200

    feat: new job card

--- desktopProbe/src/components/jobsList.tsx
+++ desktopProbe/src/components/jobsList.tsx
@@ -1,7 +1,9 @@
 import { Icons } from '@/components/icons';
+import { useLinks } from '@/hooks/links';
 import { useSites } from '@/hooks/sites';
 import { LABEL_COLOR_CLASSES } from '@/lib/labels';
 import { cn } from '@/lib/utils';
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
 import { ArchiveIcon, TrashIcon } from '@radix-ui/react-icons';
 import { createRef, useEffect, useMemo, useState } from 'react';
 import { useHotkeys } from 'react-hotkeys-hook';
@@ -33,11 +35,13 @@ export function JobsList({
   onDelete: (job: Job) => void;
 }) {
   const { siteLogos } = useSites();
+  const { links } = useLinks();
 
   const [jobToDelete, setJobToDelete] = useState<Job | undefined>();
   const [scrollToIndex, setScrollToIndex] = useState<number | undefined>();
   const itemRefs = useMemo(() => jobs.map(() => createRef<HTMLLIElement>()), [jobs]);
   const selectedIndex = jobs.findIndex((job) => job.id === selectedJobId);
+  const linksMap = useMemo(() => new Map(links.map((link) => [link.id, link])), [links]);
 
   useEffect(() => {
     if (scrollToIndex === undefined) {
@@ -126,106 +130,133 @@ export function JobsList({
     >
       <ul>
         {jobs.map((job, index) => {
+          const fromLink = linksMap.get(job.link_id)?.title;
+
           return (
             <li
               key={job.id}
-              className={cn('-mt-[1px] px-2 pt-6 xl:px-4', selectedJobId === job.id && 'bg-muted')}
+              className={cn('-mt-[1px] px-2 pt-6 xl:px-5', selectedJobId === job.id && 'bg-muted')}
               ref={itemRefs[index]}
               onClick={() => onSelect(job)}
             >
-              <div className="mb-6 flex items-center gap-4">
-                {/* Company logo */}
-                <Avatar className="h-16 w-16">
-                  <AvatarImage src={siteLogos[job.siteId]} />
-                  <AvatarFallback>LI</AvatarFallback>
-                </Avatar>
+              <div className="flex flex-wrap-reverse items-center justify-between gap-1.5">
+                {/* Company Name */}
+                <p className="my-1.5 text-xs text-muted-foreground">{job.companyName}</p>
 
-                <div className="grow">
-                  <div className="flex flex-wrap items-center justify-between gap-1.5">
-                    {/* Company Name */}
-                    <p className="mb-3 text-xs text-muted-foreground">{job.companyName}</p>
+                {/* Action buttons */}
+                <div className="ml-auto flex items-center gap-2">
+                  {/* Archive button */}
+                  {job.status !== 'archived' && (
+                    <TooltipProvider delayDuration={500}>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <Button
+                            variant="secondary"
+                            className="h-[22px] w-[22px] bg-transparent px-0 transition-colors duration-200 ease-in-out hover:bg-foreground/10 focus:bg-foreground/10"
+                            onClick={(evt) => {
+                              onArchive(job);
+                              evt.stopPropagation();
+                            }}
+                          >
+                            <ArchiveIcon className="min-h-4 w-fit text-foreground" />
+                          </Button>
+                        </TooltipTrigger>
 
-                    {/* Action buttons */}
-                    <div className="ml-auto flex items-center gap-2">
-                      {/* Archive button */}
-                      {job.status !== 'archived' && (
-                        <TooltipProvider delayDuration={500}>
-                          <Tooltip>
-                            <TooltipTrigger>
-                              <Button
-                                variant="secondary"
-                                className="h-[22px] w-[22px] bg-transparent px-0 transition-colors duration-200 ease-in-out hover:bg-foreground/10 focus:bg-foreground/10"
-                                onClick={(evt) => {
-                                  onArchive(job);
-                                  evt.stopPropagation();
-                                }}
-                              >
-                                <ArchiveIcon className="min-h-4 w-fit text-foreground" />
-                              </Button>
-                            </TooltipTrigger>
+                        <TooltipContent side="bottom" className="text-base">
+                          Archive
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  )}
 
-                            <TooltipContent side="bottom" className="text-base">
-                              Archive
-                            </TooltipContent>
-                          </Tooltip>
-                        </TooltipProvider>
-                      )}
+                  {/* Delete button */}
+                  <TooltipProvider delayDuration={500}>
+                    <Tooltip>
+                      <TooltipTrigger>
+                        <Button
+                          variant="destructive"
+                          className="h-[22px] w-[22px] bg-transparent px-0 transition-colors duration-200 ease-in-out hover:bg-destructive/20 focus:bg-destructive/20"
+                          onClick={(evt) => {
+                            // onDelete(job);
+                            setJobToDelete(job);
+                            evt.stopPropagation();
+                          }}
+                        >
+                          <TrashIcon className="h-5 w-auto text-destructive" />
+                        </Button>
+                      </TooltipTrigger>
 
-                      {/* Delete button */}
-                      <TooltipProvider delayDuration={500}>
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <Button
-                              variant="destructive"
-                              className="h-[22px] w-[22px] bg-transparent px-0 transition-colors duration-200 ease-in-out hover:bg-destructive/20 focus:bg-destructive/20"
-                              onClick={(evt) => {
-                                // onDelete(job);
-                                setJobToDelete(job);
-                                evt.stopPropagation();
-                              }}
-                            >
-                              <TrashIcon className="h-5 w-auto text-destructive" />
-                            </Button>
-                          </TooltipTrigger>
-
-                          <TooltipContent side="bottom" className="text-base">
-                            Delete
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
-                    </div>
-                  </div>
-
-                  {/* Job Title */}
-                  <p className="mt-0.5 leading-5 tracking-wide">{job.title}</p>
-
-                  <div className="mt-1 flex flex-wrap items-center justify-between gap-1.5">
-                    {/* Location, JobType, Salary & Tags */}
-                    <div className="flex flex-wrap items-center gap-1.5">
-                      {job.location && <span className="text-sm text-foreground/70">{job.location}</span>}
-                      {job.jobType && <span className="text-sm text-foreground/70">| {job.jobType}</span>}
-                      {job.salary && <span className="text-sm text-foreground/70">| {job.salary}</span>}
-                      {job.tags?.map((tag) => <span className="text-sm text-foreground/70">| {tag}</span>)}
-                    </div>
-
-                    {/* Job Label */}
-                    <div
-                      className={`w-[85px] rounded-md bg-opacity-80 py-1 text-center text-xs leading-3 text-white dark:bg-opacity-60 ${
-                        LABEL_COLOR_CLASSES[job.labels[0]]
-                      }`}
-                    >
-                      {job.labels[0]}
-                    </div>
-                  </div>
-
-                  {/* Timestamp */}
-                  <p className="ml-auto mt-2 w-fit text-xs font-extralight text-foreground/90">
-                    detected {getRelativeTimeString(new Date(job.created_at))}
-                  </p>
+                      <TooltipContent side="bottom" className="text-base">
+                        Delete
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
                 </div>
               </div>
 
-              <hr className="w-full border-muted" />
+              {/* Job Title */}
+              <p className="mt-2 leading-5 tracking-wide">{job.title}</p>
+
+              <div className="mt-1.5 flex items-center justify-between gap-4">
+                {/* Location, JobType, Salary & Tags */}
+                <p className="text-sm leading-[18px] tracking-tight text-foreground/80">
+                  {job.location && <span>{job.location}</span>}
+                  {job.jobType && (
+                    <>
+                      {job.location && <span className="mx-1 text-[14px] font-light text-foreground/40"> | </span>}
+                      <span>{job.jobType}</span>
+                    </>
+                  )}
+                  {job.salary && (
+                    <>
+                      {(job.location || job.jobType) && (
+                        <span className="mx-1 text-[14px] font-light text-foreground/40"> | </span>
+                      )}
+                      <span>{job.salary}</span>
+                    </>
+                  )}
+                  {job.tags?.map((tag) => (
+                    <>
+                      {(job.location || job.jobType || job.salary) && (
+                        <span className="text-3 mx-[14px] font-light text-foreground/40"> | </span>
+                      )}
+                      <span>| {tag}</span>
+                    </>
+                  ))}
+                </p>
+
+                {/* Job Label */}
+                {job.labels[0] && (
+                  <div
+                    className={`w-[85px] flex-shrink-0 rounded-md bg-opacity-80 py-1 text-center text-xs leading-3 text-white dark:bg-opacity-60 ${
+                      LABEL_COLOR_CLASSES[job.labels[0]]
+                    }`}
+                  >
+                    {job.labels[0]}
+                  </div>
+                )}
+              </div>
+
+              <div className="mt-4 flex items-center gap-12">
+                {/* Source */}
+                {fromLink && (
+                  <p className="flex items-center gap-2 text-xs leading-3 text-foreground/80">
+                    {/* Source logo */}
+                    <Avatar className="h-6 w-6">
+                      <AvatarImage src={siteLogos[job.siteId]} />
+                      <AvatarFallback>LI</AvatarFallback>
+                    </Avatar>
+                    {fromLink}
+                  </p>
+                )}
+
+                {/* Timestamp */}
+                <p className="ml-auto w-fit flex-shrink-0 text-xs text-foreground/80">
+                  detected {getRelativeTimeString(new Date(job.created_at))}
+                </p>
+              </div>
+
+              <hr className="mt-6 w-full border-muted" />
             </li>
           );
         })}
