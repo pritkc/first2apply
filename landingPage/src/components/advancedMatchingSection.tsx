492b09f33b7dad82a2047ef954f305074ac0c4d0
sebestindragos <sebestin.dragos@gmail.com>
2025-03-03 19:59:50 +0100

    fix(landingPage): escape quotes

--- landingPage/src/components/advancedMatchingSection.tsx
+++ landingPage/src/components/advancedMatchingSection.tsx
@@ -15,8 +15,8 @@ export function AdvancedMatchingSection() {
         </h2>
         <p className="mt-2 text-balance sm:mt-4 sm:text-lg md:text-right">
           With AI-powered search you can set smart, <span className="text-primary">natural language</span> rules to
-          refine job listings like: "exclude jobs that require office visits, even if they’re labeled remote" , "hide
-          fullstack roles that require python", "only see positions with less than 2 years of experience" and more.
+          refine job listings like: &quot;exclude jobs that require office visits, even if they’re labeled remote&quot; , &quot;hide
+          fullstack roles that require python&quot;, &quot;only see positions with less than 2 years of experience&quot; and more.
           <span className="mt-3 block">No more sifting through irrelevant listings, just jobs that suit you.</span>
         </p>
       </div>
