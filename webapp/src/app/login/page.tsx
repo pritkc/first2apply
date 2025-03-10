68ff3996c47328767a722cf56a4b4d70cbbe6450
mariaantici <maria.antici99@gmail.com>
2025-03-10 18:19:12 +0100

    refactor(webapp): remove login/signup layouts and update styling

--- webapp/src/app/login/page.tsx
+++ webapp/src/app/login/page.tsx
@@ -65,64 +65,66 @@ export default function LoginPage() {
   };
 
   return (
-    <Card className="min-w-80 space-y-2.5">
-      <CardHeader className="space-y-1">
-        <CardTitle className="text-center text-2xl tracking-wide">Log in</CardTitle>
-        <CardDescription className="text-center">
-          Don't have an account?{' '}
-          <Link href="/signup" className="text-primary hover:underline">
-            Sign up
-          </Link>
-        </CardDescription>
-      </CardHeader>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <CardContent className="space-y-2.5">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem className="space-y-1">
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input id="email" type="email" placeholder="name@example.com" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem className="space-y-1">
-                  <FormLabel>Password</FormLabel>
-                  <FormControl className="flex gap-2">
-                    <Input id="password" type="password" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </CardContent>
-          <CardFooter className="flex flex-col gap-4 pb-7 pt-2">
-            <Button className="w-full" disabled={!form.formState.isValid || isSubmitting}>
-              {isSubmitting ? (
-                <>
-                  <Icons.spinner2 className="mr-1 h-4 w-4 animate-spin" />
-                  Logging in
-                </>
-              ) : (
-                'Log in'
-              )}
-            </Button>
+    <div className="flex h-[calc(100vh-96px)] w-full items-center justify-center md:pb-14">
+      <Card className="w-full max-w-sm space-y-2.5 xxs:min-w-80">
+        <CardHeader className="space-y-1 px-0 xxs:px-6">
+          <CardTitle className="text-center text-2xl tracking-wide">Log in</CardTitle>
+          <CardDescription className="text-center">
+            Don't have an account?{' '}
+            <Link href="/signup" className="text-primary hover:underline">
+              Sign up
+            </Link>
+          </CardDescription>
+        </CardHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <CardContent className="space-y-2.5 px-0 xxs:px-6">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="space-y-1">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input id="email" type="email" placeholder="name@example.com" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="space-y-1">
+                    <FormLabel>Password</FormLabel>
+                    <FormControl className="flex gap-2">
+                      <Input id="password" type="password" {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+            <CardFooter className="flex flex-col gap-4 px-0 pb-7 pt-2 xxs:px-6">
+              <Button className="w-full" disabled={!form.formState.isValid || isSubmitting}>
+                {isSubmitting ? (
+                  <>
+                    <Icons.spinner2 className="mr-1 h-4 w-4 animate-spin" />
+                    Logging in
+                  </>
+                ) : (
+                  'Log in'
+                )}
+              </Button>
 
-            <div className="justify-self-end">
-              <Link href="/forgot-password" className="w-fit text-xs text-muted-foreground hover:underline">
-                Forgot password?
-              </Link>
-            </div>
-          </CardFooter>
-        </form>
-      </Form>
-    </Card>
+              <div className="justify-self-end">
+                <Link href="/forgot-password" className="w-fit text-xs text-muted-foreground hover:underline">
+                  Forgot password?
+                </Link>
+              </div>
+            </CardFooter>
+          </form>
+        </Form>
+      </Card>
+    </div>
   );
 }
