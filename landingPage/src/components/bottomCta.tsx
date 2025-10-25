728089651d0dfd368088452f14085b9b84b95fc3
sebestindragos <sebestin.dragos@gmail.com>
2025-10-25 22:12:30 +0200

    fix(landingPage): update number of users

--- landingPage/src/components/bottomCta.tsx
+++ landingPage/src/components/bottomCta.tsx
@@ -1,22 +1,17 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
+import Link from 'next/link';
+
+import { Button } from './ui/button';
 
 export function BottomCta() {
   return (
-    <section
-      id="bottom-cta"
-      className="max-w-7xl mx-auto px-6 sm:px-10 pt-[10vh] md:pt-[20vh]"
-    >
-      <h2 className="text-2xl sm:text-4xl lg:text-5xl font-semibold md:text-center">
-        Ready to get started?
-      </h2>
+    <section id="bottom-cta" className="mx-auto max-w-7xl px-6 pt-[10vh] sm:px-10 md:pt-[20vh]">
+      <h2 className="text-2xl font-semibold sm:text-4xl md:text-center lg:text-5xl">Ready to get started?</h2>
 
-      <p className="mt-2 sm:mt-6 md:mx-auto w-fit max-w-[600px] lg:max-w-[800px] md:text-center">
-        Join 1000+ users who have already found their dream job with First 2
-        Apply.
+      <p className="mt-2 w-fit max-w-[600px] sm:mt-6 md:mx-auto md:text-center lg:max-w-[800px]">
+        Join 7500+ users who have tried First 2 Apply to streamline their job applications and land their dream jobs.
       </p>
 
-      <Link href="/download" className="flex justify-center mt-6 md:mt-12">
+      <Link href="/download" className="mt-6 flex justify-center md:mt-12">
         <Button size="lg" className="w-full xs:w-fit">
           Download for free
         </Button>
